8d77160abef92ba7b2489db08926f3d6eb96382d
Shivamyadav-11 <shivamyadavd.10@gmail.com>
2026-08-22 23:05:17 +0530

    fix import paths

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import Login from "./pages/Login";
 import About from "./pages/About";
@@ -8,7 +7,7 @@ import Contact from "./pages/Contact";
 import MyProfile from "./pages/MyProfile";
 import MyAppointments from "./pages/MyAppointments";
 import Appointment from "./pages/Appointment";
-import Navbar from "./components/NAvbar";
+import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
